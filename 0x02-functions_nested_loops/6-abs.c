24348a951f9d2a77199b929de6b67d344b1c69fb
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-06-20 09:22:57 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value
+ * @n : number to check
+ * Return:0(success) or 1(error)
+ */
+
+int _abs(int n)
+{
+
+return (n >= 0 ? n : n * -1);
+
+}
