578a43685c511edac1892bdcc763223f4fec6740
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-06-20 07:50:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_alphabet - prints the alphabet a - z
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
