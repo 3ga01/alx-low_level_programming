7d99fd30f7a8dbd6c24ebe7e92cf409bb54aaf81
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-06-26 10:41:53 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by newline
+ *
+ * @str: string to be printed
+ *
+ * Return: void(success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
