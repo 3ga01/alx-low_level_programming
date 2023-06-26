f3e8417878270bd8932bf48d38d0b1e9963191cf
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-06-26 10:26:46 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ *
+ * @s: Memory address of the string
+ *
+ * Return: int(success)
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
