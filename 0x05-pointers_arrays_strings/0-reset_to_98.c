20ecf697b3dc1d94ea0763ef915d823191560d89
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-06-26 09:51:42 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an integer value to 98
+ *
+ * @n: The pointer to the integer that will be updated
+ *
+ * Return: void(success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
